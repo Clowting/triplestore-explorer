1949419e22b280fc2a1d8b9b623ddfa06014c908
Raymon de Looff <raydelooff@gmail.com>
2015-11-26 11:57:00 +0100

    Added test routes

--- src/main/java/triplestoreexplorer/App.java
+++ src/main/java/triplestoreexplorer/App.java
@@ -2,13 +2,19 @@ package triplestoreexplorer;
 
 import static spark.Spark.*;
 
+import spark.ModelAndView;
+import triplestoreexplorer.controller.DatasetsPageController;
+import triplestoreexplorer.model.DatasetsPageModel;
+import triplestoreexplorer.template.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A Triplestore web application
  * @author Raymon de Looff, Thijs Clowting
  */
 public class App {
-
-    private static String title = "A web-based Triplestore explorer";
 
     public App() {
 
@@ -24,6 +30,17 @@ public class App {
 
         // Define routes
         get("/", (req, res) -> "Hello World");
+
+        Map map = new HashMap();
+        map.put("name", "Sam");
+
+        // Datasets
+        DatasetsPageModel datasetsPageModel = new DatasetsPageModel();
+        DatasetsPageController datasetsPageController = new DatasetsPageController();
+        get("/datasets", (request, response) -> new ModelAndView(map, "datasets"), new HandlebarsTemplateEngine());
+
+        // Handlebars test
+        get("/test", (rq, rs) -> new ModelAndView(map, "base"), new HandlebarsTemplateEngine());
 
     }
 
