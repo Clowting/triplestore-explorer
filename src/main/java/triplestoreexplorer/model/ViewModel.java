0d779c7827ad0e5f7c366eebb191fb2a8e2e99c7
Raymon de Looff <raydelooff@gmail.com>
2015-11-29 12:20:47 +0100

    Changed fields

--- src/main/java/triplestoreexplorer/model/ViewModel.java
+++ src/main/java/triplestoreexplorer/model/ViewModel.java
@@ -9,56 +9,58 @@ import java.util.Map;
  */
 public class ViewModel {
 
-    private String title;
-    private Map<String, String> parameters;
     private Map<String, Map> data;
 
     /**
      * The default constructor
      */
     public ViewModel() {
-        parameters = new HashMap<String, String>();
+        data = new HashMap<String, Map>();
+
+        // Add view data
+        HashMap<String, String> viewData = new HashMap<String, String>();
+        data.put("view", viewData);
     }
 
     /**
-     * Returns the title of the webpage
-     * @return The title of the webpage
+     * Returns the title of the view
+     * @return The title of the view
      */
     public String getTitle() {
-        return title;
+        return (String) data.get("view").get("title");
     }
 
     /**
-     * Sets the title of the webpage
-     * @param title The title of the webpage
+     * Sets the title of the view
+     * @param title The title of the view
      */
     public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    /**
-     * Returns the parameters of the request
-     * @return The parameters of the request
-     */
-    private Map<String, String> getParameters() {
-        return parameters;
+        data.get("view").put("title", title);
     }
 
     /**
-     * Sets the parameters of the request
-     * @param parameters The parameters of the request
-     */
-    public void setParameters(Map<String, String> parameters) {
-        this.parameters = parameters;
-    }
-
-    /**
-     * This method returns the data stored in the model
+     * Returns the data stored in the model
      * @return The data stored in the model
      */
     public Map<String, Map> getData() {
         return data;
+    }
+
+    /**
+     * Sets the data in the model
+     * @param data The data to set in the model
+     */
+    public void setData(Map<String, Map> data) {
+        this.data = data;
+    }
+
+    /**
+     * Adds the given map to the data in the model
+     * @param key The key to the value
+     * @param map The Map to store in the model
+     */
+    public void addData(String key, Map map) {
+        data.put(key, map);
     }
 
 }
